0e97f21bde8854310aa6f029d7dba414b77edc3d
rffffffff007 <rffffffff007@gmail.com>
2014-12-14 15:14:44 +0800

    Add fake data.

--- photoshareserver/src/main/java/com/example/photoshare/api/PhotoService.java
+++ photoshareserver/src/main/java/com/example/photoshare/api/PhotoService.java
@@ -7,6 +7,7 @@ import org.apache.thrift.TException;
 
 import com.example.photoshare.thrift.AException;
 import com.example.photoshare.thrift.Feed;
+import com.example.photoshare.thrift.FeedList;
 import com.example.photoshare.thrift.IPhotoService;
 
 public class PhotoService extends BaseServlet {
@@ -19,6 +20,17 @@ public class PhotoService extends BaseServlet {
     }
 
     public static class PhotoServiceImpl implements IPhotoService.Iface {
+        List<Feed> feeds = new ArrayList<Feed>();
+        
+        protected PhotoServiceImpl() {
+            feeds = new ArrayList<Feed>();
+            Feed feed = new Feed();
+            feed.setFeed_id("test1");
+            feed.setUser_name("robot");
+            feed.setFeed_desc("This is for test");
+            feed.setPhoto_url("http://211.155.92.122:8080/images/test.jpg");
+            feeds.add(feed);
+        }
         
         @Override
         public String hello(String name) throws AException, TException {
@@ -26,21 +38,30 @@ public class PhotoService extends BaseServlet {
         }
 
         @Override
-        public List<Feed> getFeedList(int page, int page_count)
+        public FeedList getFeedList(int page_num, int page_count)
                 throws AException, TException {
-            List<Feed> feeds = new ArrayList<Feed>();
-            Feed feed = new Feed();
-            feed.setFeed_id("test1");
-            feed.setUser_name("robot");
-            feed.setFeed_desc("This is for test");
-            feed.setPhoto_url("http://211.155.92.122:8080/images/test.jpg");
-            feeds.add(feed);
-            return feeds;
+            if (page_count <= 0) {
+                throw new AException("page_count should be bigger than 0");
+            }
+            FeedList feedList = new FeedList();
+            feedList.setPage_num(page_num);
+            feedList.setPage_count(page_count);
+            int totalPageNum = (feeds.size() + page_count - 1) / page_count;
+            feedList.setTotal_page_num(totalPageNum);
+            if (page_num > totalPageNum) {
+                throw new AException("There are only " + totalPageNum + " pages.");
+            }
+            int endPos = feeds.size() - page_num * page_count;
+            int startPos = endPos - page_count;
+            startPos = startPos < 0 ? 0 : startPos;
+            feedList.setFeeds(feeds.subList(startPos, endPos));
+            return feedList;
         }
 
         @Override
         public Feed uploadFeed(Feed feed) throws AException, TException {
-            feed.setFeed_id("new generated id");
+            feed.setFeed_id("" + System.currentTimeMillis());
+            feeds.add(feed);
             return feed;
         }
 
--- photoshareserver/src/test/java/com/example/photoshare/api/PhotoServiceTest.java
+++ photoshareserver/src/test/java/com/example/photoshare/api/PhotoServiceTest.java
@@ -2,9 +2,57 @@ package com.example.photoshare.api;
 
 import junit.framework.TestCase;
 
-public class PhotoServiceTest extends TestCase {
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.THttpClient;
+import org.apache.thrift.transport.TTransport;
 
-	public void testHello() {
-		assertTrue(true);
-	}
+import com.example.photoshare.thrift.AException;
+import com.example.photoshare.thrift.Feed;
+import com.example.photoshare.thrift.FeedList;
+import com.example.photoshare.thrift.IPhotoService;
+
+public class PhotoServiceTest extends TestCase {
+    private IPhotoService.Client mPhotoClient;
+    private TTransport mTransport;
+    private static final String API_ADDRESS = "http://211.155.92.122:8080/photoshare/photoservice";
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mTransport = new THttpClient(API_ADDRESS);
+        mTransport.open();
+        TProtocol protocol = new TBinaryProtocol(mTransport);
+        mPhotoClient = new IPhotoService.Client(protocol);
+
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        mTransport.close();
+    }
+
+    public void testHello() throws AException, TException {
+        String val = mPhotoClient.hello("faylon");
+        assertEquals(val, "Hello faylon");
+    }
+    
+    public void testGetFeedList() throws AException, TException {
+        FeedList feedList = mPhotoClient.getFeedList(0, 10);
+        for (Feed feed : feedList.getFeeds()) {
+            System.out.println(feed.toString());
+        }
+        assertTrue(feedList.getFeedsSize() > 0);
+    }
+    
+    public void testUploadFeed() throws AException, TException {
+        Feed feed = new Feed();
+        feed.user_name = "testcase";
+        feed.photo_url = "http://www.baidu.com";
+        feed.feed_desc = "unit test " + System.currentTimeMillis();
+        Feed resFeed = mPhotoClient.uploadFeed(feed);
+        assertTrue(resFeed.isSetFeed_id());
+    }
 }
