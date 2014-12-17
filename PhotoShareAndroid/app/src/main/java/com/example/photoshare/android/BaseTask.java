ac5037091c8eaffb7c5516582b49382fb11e2df6
Zhijian He <hezhijian@hezhijian-macbookpro2.roam.corp.google.com>
2014-12-17 14:57:15 +0800

    add the RefreshFeedsTask logic

---
+++ PhotoShareAndroid/app/src/main/java/com/example/photoshare/android/BaseTask.java
@@ -0,0 +1,47 @@
+package com.example.photoshare.android;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.content.Context;
+
+import android.widget.Toast;
+
+import com.example.photoshare.thrift.AException;
+
+import org.apache.thrift.TException;
+
+/**
+ * Created by hezhijian on 12/17/14.
+ */
+abstract class BaseTask extends AsyncTask<Void, Void, Object> {
+    private ProgressDialog mProgress;
+    protected Context mContext;
+
+    public BaseTask(Context context) {
+        mContext = context;
+        mProgress = new ProgressDialog(context);
+        mProgress.setTitle("Loading");
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        if (!mProgress.isShowing()) {
+            mProgress.show();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        super.onPostExecute(o);
+        if (mProgress.isShowing()) {
+            mProgress.dismiss();
+        }
+        if (o instanceof AException) {
+            String msg = ((AException) o).getMessage();
+            Toast.makeText(mContext, msg, Toast.LENGTH_SHORT).show();
+        } else if (o instanceof TException) {
+            Toast.makeText(mContext, "Cannot connect to internet.", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
